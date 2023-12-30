8d90bb13ffa409926b0cd8e283eacb4eb70af65f
Valentin <99755643+Valentin79@users.noreply.github.com>
2023-12-30 15:56:39 +0300

    sem_7

---
+++ Soiution2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using ChatApp;
+
+namespace Solution
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                var s = new Server<IPEndPoint>(new UdpMessageSourceServer());
+                await s.Start();
+            }
+            else
+            if (args.Length == 1)
+            {
+                var c = new Client<IPEndPoint>(new UdpMessageSourceClient(), args[0]);
+                await c.Start();
+            }
+            else
+            {
+
+                Console.WriteLine("Для запуска сервера введите ник-нейм как параметр запуска приложения");
+                Console.WriteLine("Для запуска клиента введите ник-нейм и IP сервера как параметры запуска приложения");
+            }
+            Console.ReadKey();
+        }
+    }
+}
